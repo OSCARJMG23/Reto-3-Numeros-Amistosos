5e0440464750305c7d519ade6b5ad94215cdfbfc
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-12-04 20:16:21 -0500

    feat: :sparkles: Test Completed

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int rango1 = 1000;
+int rango2 = 1500;
+
+ObtenerNumerosAmistosos(rango1, rango2);
+
+
+static void ObtenerNumerosAmistosos(int inicio, int fin)
+{
+    for (int m = inicio; m <= fin; m++)
+    {
+        int n = SumaDivisores(m);
+
+        if (n != m && SumaDivisores(n) == m)
+        {
+            Console.WriteLine($"Par amigable encontrado: ({m}, {n})");
+        }
+    }
+}
+
+static int SumaDivisores(int numero)
+{
+    int suma = 0;
+    for (int i = 1; i < numero; i++)
+    {
+        if (numero % i == 0)
+        {
+            suma += i;
+        }
+    }
+    return suma;
+}
